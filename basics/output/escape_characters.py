7b2e5812c705789039c62033e714f7f971a0437d
repl.it user <replituser@example.com>
2020-10-01 09:34:00 +0000

    added code to display escape characters

---
+++ basics/output/escape_characters.py
@@ -0,0 +1,6 @@
+#display escape characters.py
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
