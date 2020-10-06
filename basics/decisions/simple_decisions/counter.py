f9b3287b90852b190abd44da3bf90925029b8a7a
repl.it user <replituser@example.com>
2020-10-06 10:19:26 +0000

    counter

---
+++ basics/decisions/simple_decisions/counter.py
@@ -0,0 +1,23 @@
+number1 = int(input("Please enter a first number "))
+number2 = int(input("Please enter a second number "))
+number3 = int(input("Please enter a third number "))
+
+even = 0
+odd = 0 
+
+if (number1 % 2) == 0:
+  even = even + 1
+else:
+  odd = odd + 1
+
+if (number2 % 2) == 0:
+  even = even + 1
+else:
+  odd = odd + 1
+
+if (number3 % 2) == 0:
+  even = even + 1
+else:
+  odd = odd + 1
+
+print("There are {} even numbers and {} odd numbers".format(even, odd))
