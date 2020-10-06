69683cb194ce3c90b3f4c08f97e2019757498793
repl.it user <replituser@example.com>
2020-10-06 09:47:00 +0000

    modulo operator

---
+++ basics/decisions/simple_decisions/modulo_operator.py
@@ -0,0 +1,8 @@
+number = int(input("Please enter a whole number "))
+
+calculated_number = (number % 2)
+
+if calculated_number == 0:
+  print(number, "is an even number!")
+else:
+  print(number, "is an odd number!")
