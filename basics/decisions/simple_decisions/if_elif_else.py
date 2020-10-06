ccb0c8533cd89ecaa17b4092c8169713a4c92d36
repl.it user <replituser@example.com>
2020-10-06 09:36:25 +0000

    if elif else statement

---
+++ basics/decisions/simple_decisions/if_elif_else.py
@@ -0,0 +1,12 @@
+direction = input("Which direction should i paint? (up, down, left or right) ")
+
+if direction == "up":
+  print("I am painting in the upwards direction!")
+elif direction == "down":
+  print("I am painting in the downwards direction!")
+elif direction == "left":
+  print("I am painting in the leftwards direction!")
+elif direction == "right":
+  print("I am painting in the rightwards direction!")
+else:
+  print("Please enter a valid answer!")
