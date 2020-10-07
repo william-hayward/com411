be7e2f555206debfe02a24baffdc0a3268c53763
repl.it user <replituser@example.com>
2020-10-07 17:59:27 +0000

    added code to demonstrate the use of one loop nested in another

---
+++ basics/repetitions/nested_loop/nested.py
@@ -0,0 +1,9 @@
+columns = int(input("How many columns should i have? "))
+rows = int(input("How many rows should i have? "))
+emoji = ":-)"
+
+for i in range(0, columns,1):
+  for i in range(0, rows, 1):
+    print(emoji, end="")
+  print()
+print("\nDone!")
