920089b02c15f9e18327632efc4fe255bbc2a51e
repl.it user <replituser@example.com>
2020-10-07 16:37:25 +0000

    added code to demonstrate the use of a simple for loop

---
+++ basics/repetitions/for_loop/simple.py
@@ -0,0 +1,10 @@
+mountains = int(input("How many mountains should i display? "))
+for i in range(mountains):
+  print("           __")
+  print("          /  \_")  
+  print("         /^    \\")
+  print("        /  ^    \_")
+  print("      _/ ^ ^     ^\\")
+  print("     /  ^     ^    \\")
+
+  
