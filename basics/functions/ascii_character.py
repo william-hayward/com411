6130fa49dc71cf59ad1abe6b08d68d2d976b6bb8
WilliamHayward2 <5hayww75@solent.ac.uk>
2020-10-13 09:14:37 +0000

    added code to demonstrate the use of built in functions: chr()

---
+++ basics/functions/ascii_character.py
@@ -0,0 +1,4 @@
+print("Program Started!\n")
+code = int(input("Please enter an ASCII code: "))
+print("The character represented by the ASCII code {} is: {}".format(code, chr(code)))
+print("\nProgram Ended!")
