f61565d124f98d48f2e72b41032b55578dc175b4
WilliamHayward2 <5hayww75@solent.ac.uk>
2020-10-27 12:44:45 +0000

    sorted set

--- data/sets/sorted_set.py
+++ data/sets/sorted_set.py
@@ -7,26 +7,28 @@ def run():
     return observations
   
   def remove_observations(observations):
+    running = True
     while running == True:
       response = input("Do you wish to remove an observation (yes/no)? ")
       if response == "yes":
         observation_to_remove = input("What observation do you wish to remove? ")
-        observations_set.discard(observation_to_remove)
+        observations.remove(observation_to_remove)
         print("Done!")
       elif response == "no":
-        return observations_set
+        return observations
         running == False
       else:
         print("Enter a valid answer")
   
-  running = True
+  
   print("Counting observations...")
   observations = observed()
+  remove_observations(observations)
   observations_set = set()
   
   for count in observations:
     observations_set.add((count, observations.count(count)))
-  
-  remove_observations(observations)
+    
   print("\nObservations: ")
-  print(observations_set)
+  for count, repeats in observations_set:
+    print("{} observed {} times.".format(count, repeats))
